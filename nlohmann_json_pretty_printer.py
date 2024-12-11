827aca34966443f4552b55890fb6c039e3f12be3
Michael Cache <faith_gentoo@163.com>
2024-12-11 19:06:34 +0800

    <refactor>refactor json object printer

--- nlohmann_json_pretty_printer.py
+++ nlohmann_json_pretty_printer.py
@@ -1,61 +1,69 @@
 import gdb
 import re
 
-nlohmann_json_type_regex = re.compile(r"nlohmann::.*?basic_json<.*")
 
 def register_printer_gen(obj=None):
     gdb.printing.register_pretty_printer(obj, NlohmannJsonPrinterGen())
 
+
 class NlohmannJsonPrinterGen(gdb.printing.PrettyPrinter):
+    nlohmann_json_type_regex = re.compile(r"nlohmann::.*?basic_json<.*")
+
     def __init__(self):
         super().__init__('nlohmann_json_pretty_printer')
         self.enabled = True
-        
+
     def __call__(self, val):
         t = gdb.types.get_basic_type(val.type)
-        if re.match(nlohmann_json_type_regex, str(t)):
+        if re.match(NlohmannJsonPrinterGen.nlohmann_json_type_regex, str(t)):
             json_type = str(val["m_data"]['m_type'])
-            if json_type.endswith("object") or json_type.endswith("array"):
-                return NlohmannJsonConainerPrinter(val)
+            if json_type.endswith("object"):
+                return NlohmannJsonObjectPrinter(val)
+            elif json_type.endswith("array"):
+                return NlohmannJsonArrayPrinter(val)
             elif json_type.endswith("string") or json_type.endswith("boolean") or \
-                json_type.endswith("number_integer") or \
-                json_type.endswith("number_unsigned") or \
-                json_type.endswith("number_float") or \
-                json_type.endswith("number") or json_type.endswith("null"):
+                    json_type.endswith("number_integer") or \
+                    json_type.endswith("number_unsigned") or \
+                    json_type.endswith("number_float") or \
+                    json_type.endswith("number") or json_type.endswith("null"):
                 return NlohmannJsonPrinter(val)
             else:
                 return None
         else:
-            return None 
-    
+            return None
 
-class NlohmannJsonConainerPrinter:
+
+class NlohmannJsonObjectPrinter:
     def __init__(self, value):
         self.value = value
         self.json_type = str(value["m_data"]['m_type'])
 
     def to_string(self):
-        if self.json_type.endswith("object"):
-            return "object"
-        elif self.json_type.endswith("array"):
-            return "array"
-        else:
-            return "not supported json type:{}".format(self.json_type)
-    
+        return "object"
+
     def children(self):
-        if self.json_type.endswith("object"):
-            js_map = self.value["m_data"]["m_value"]["object"].dereference()
-            map_printer = gdb.default_visualizer(js_map)
-            map_children = map_printer.children()
-            return map_children
-        elif self.json_type.endswith("array"):
-            js_array = self.value["m_data"]["m_value"]["array"].dereference()
-            array_printer = gdb.default_visualizer(js_array)
-            array_children = array_printer.children()
-            return array_children
-        else:
-            return ()
-        
+        js_map = self.value["m_data"]["m_value"]["object"].dereference()
+        map_printer = gdb.default_visualizer(js_map)
+        map_iter = iter(map_printer.children())
+        while True:
+            yield str(map_iter.__next__()[1]).strip('"'), map_iter.__next__()[1]
+
+
+class NlohmannJsonArrayPrinter:
+    def __init__(self, value):
+        self.value = value
+        self.json_type = str(value["m_data"]['m_type'])
+
+    def to_string(self):
+        return "array"
+
+    def children(self):
+        js_array = self.value["m_data"]["m_value"]["array"].dereference()
+        array_printer = gdb.default_visualizer(js_array)
+        array_children = array_printer.children()
+        return array_children
+
+
 class NlohmannJsonPrinter:
     def __init__(self, value):
         self.value = value
@@ -77,5 +85,3 @@ class NlohmannJsonPrinter:
         else:
             print(self.json_type)
             return "not supported json type:{}".format(self.json_type)
-    
-                  
